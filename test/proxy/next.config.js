e554f11411d94dcdc106231e8db18869b9b9ef10
Alejandro Tardín <alejandro@tardin.com>
2022-03-14 04:58:05 +0100

    Always returning the same function
    Fixes #38.

--- test/proxy/next.config.js
+++ test/proxy/next.config.js
@@ -1,6 +1,18 @@
+const path = require('path')
 const { withPlausibleProxy } = require('../../dist')
 
 module.exports = withPlausibleProxy()({
+  webpack: (config) => {
+    config.resolve.alias['react'] = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'node_modules',
+      'react'
+    )
+
+    return config
+  },
   images: {
     domains: ['github.githubassets.com'],
   },
