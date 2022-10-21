5773b05c1bf47f6573787d84df3e452842da2f79
Alejandro Tardín <alejandro@tardin.com>
2022-10-22 00:15:09 +0200

    Showing likes for the last 12months

--- demo/pages/api/like.js
+++ demo/pages/api/like.js
@@ -2,7 +2,7 @@ export default async function likeHandler(_, res) {
   res.send({
     likes: process.env.PLAUSIBLE_API_KEY
       ? await fetch(
-          'https://plausible.io/api/v1/stats/breakdown?site_id=next-plausible.vercel.app&property=event:name&metrics=events&filters=event:name==Like',
+          'https://plausible.io/api/v1/stats/breakdown?site_id=next-plausible.vercel.app&property=event:name&metrics=events&filters=event:name==Like&period=12mo',
           {
             headers: {
               Authorization: `Bearer ${process.env.PLAUSIBLE_API_KEY}`,
