6cbab71011e40a84d4fb712c1d51dd562339f96d
Tim Raderschad <tim.raderschad@gmail.com>
2023-08-14 14:00:00 +0000

    fix: usage of events with type never

--- test/unit/usePlausible.test.ts
+++ test/unit/usePlausible.test.ts
@@ -8,11 +8,18 @@ import { renderHook } from '@testing-library/react'
 describe('usePlausible', () => {
   it('is type safe', () => {
     type Events = {
+      someEvent: never
       e: { a: string; b: number }
     }
     const {
       result: { current: plausible },
     } = renderHook(() => usePlausible<Events>())
+
+    // happy cases -> correct function calls
+    plausible('someEvent')
+    plausible('e', { props: { a: 'a', b: 0 } })
+
+    // falsy function calls -> typescript will error here
 
     // @ts-expect-error
     plausible('e')
@@ -24,6 +31,5 @@ describe('usePlausible', () => {
     plausible('e', { props: { a: 'a', b: 'b' } })
     // @ts-expect-error
     plausible('a', { props: { a: 'a', b: 0 } })
-    plausible('e', { props: { a: 'a', b: 0 } })
   })
 })
