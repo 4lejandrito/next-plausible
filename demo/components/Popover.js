ecbd0d33fdd63f7acef41c94d9236444771f6a58
Alejandro Tardín <alejandro@tardin.com>
2025-11-10 07:39:43 +0100

    Replacing popper with floating-ui

--- demo/components/Popover.js
+++ demo/components/Popover.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useFloating, autoUpdate, offset, arrow } from '@floating-ui/react'
 import { Transition } from '@tailwindui/react'
 
 export default function Popover({ open, content, children }) {
+  const [arrowElement, setArrowElement] = useState(null)
+  const { refs, floatingStyles, middlewareData } = useFloating({
+    placement: 'top-end',
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset({ mainAxis: 10, crossAxis: 16 }),
+      arrow({ element: arrowElement }),
+    ],
+  })
   return (
-    <span className="relative inline-flex">
-      {children}
+    <>
+      <span ref={refs.setReference}>{children}</span>
       <Transition
         as="span"
         show={open}
@@ -15,17 +25,24 @@ export default function Popover({ open, content, children }) {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <div className="absolute -top-2 right-0 -translate-y-full">
+        <div ref={refs.setFloating} style={floatingStyles}>
           <div className="px-4 relative">
             <div className="bg-help text-copy text-sm rounded text-left px-2 py-1 font-medium relative z-10 max-w-sm">
               {content}
             </div>
-            <div className="relative z-0 h-4 w-full">
-              <div className="h-4 w-4 bg-help rounded rotate-45 absolute right-4 -top-2" />
+            <div
+              className="z-0 absolute"
+              ref={setArrowElement}
+              style={{
+                left: middlewareData.arrow?.x,
+                top: middlewareData.arrow?.y,
+              }}
+            >
+              <div className="h-4 w-4 bg-help rounded rotate-45 -translate-y-1/2 relative -top-1" />
             </div>
           </div>
         </div>
       </Transition>
-    </span>
+    </>
   )
 }
